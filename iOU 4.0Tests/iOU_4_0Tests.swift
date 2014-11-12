25d8893679ae2760810b998b7147f2df78b85146
Dominick Hera <dominickhera@Dominicks-MBP-2.attlocal.net>
2014-11-11 20:47:08 -0500

    Initial Commit

---
+++ iOU 4.0Tests/iOU_4_0Tests.swift
@@ -0,0 +1,36 @@
+//
+//  iOU_4_0Tests.swift
+//  iOU 4.0Tests
+//
+//  Created by Dominick Hera on 11/11/14.
+//  Copyright (c) 2014 Posa. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class iOU_4_0Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
